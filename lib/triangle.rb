cbe1e2771e7e2a3a28f513a2494e69ec8f010708
Alexander Carlin <ahcarlin@gmail.com>
2018-10-15 16:58:04 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,26 @@
 class Triangle
-  # write code here
+  attr_accessor :side1, :side2, :side3
+  
+  def initialize(side1, side2, side3)
+    self.side1 = side1
+    self.side2 = side2
+    self.side3 = side3
+  end
+  
+  def kind
+    sides = [side1, side2, side3]
+    sides.sort!
+    if ((sides[0] + sides[1]) <= sides[2])  || ((side1 <= 0 || side2 <= 0) || side3 <= 0) 
+        raise TriangleError.new("Please enter a valid triangle")
+    elsif side1 == side2 && side2 == side3
+      return :equilateral
+    elsif (side1 == side2 || side1 == side3) || side2 == side3
+      return :isosceles
+    else 
+      return :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+  end
 end
